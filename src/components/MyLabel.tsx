0f85bda9a6b53f389cf51e23be75332cb7695ef3
Victor Azuara <vzuara@gmail.com>
2022-05-29 20:17:43 -0700

    Primer Commit

---
+++ src/components/MyLabel.tsx
@@ -0,0 +1,42 @@
+import './mylabel.css'
+
+export interface MyLabelProps {
+  /**
+   * Esta es la etiqueta que se mostrará
+   */
+  label: string;
+  /**
+   * Estos son los tamaños que se pueden usar
+   */
+  size: 'normal'|'h1'|'h2'|'h3';
+  /**
+   * Pone en mayusculas todo el texto
+   */
+   allCaps?: boolean;
+   /**
+   * Estos son los colores que se pueden usar
+   */
+   color?: 'primary'|'secondary'|'tertiary'
+   /**
+   * Color personalizado de la fuente
+   */
+    fontColor?: string
+  
+}
+
+export const MyLabel = ({
+  allCaps = false,
+  color   = 'primary',
+  label   = 'No Label',
+  size    = 'normal',
+  fontColor
+}: MyLabelProps) => {
+  return (
+    <span
+      className={`label ${size} text-${color}`}
+      style={{color: fontColor}}
+    >
+        { allCaps ? label.toUpperCase() : label }
+    </span>
+  )
+}
